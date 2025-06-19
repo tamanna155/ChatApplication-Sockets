8997b51fdc8c3fc418f23a4afed9c083f8485871
Tamanna Tripathy <87202498+tamanna155@users.noreply.github.com>
2025-06-19 16:10:45 +0530

    Updated

--- public/vite.config.js
+++ public/vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills(),
-  ],
+  plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
-  }
-})
+  },
+  base: '/',
+});
